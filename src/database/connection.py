5db5e15164ad8664e95bb1bf53ab6766c179fd80
ricardo-ervilha <ricardoervilha456@gmail.com>
2025-02-16 01:24:29 -0300

    Update connection.py

--- src/database/connection.py
+++ src/database/connection.py
@@ -7,6 +7,6 @@ from pymongo import MongoClient
 load_dotenv()
 
 mongo_client = MongoClient(
-    getenv('MONGO_URL', "mongodb+srv://ricardoervilha:password1234@mongo-eh-lixoo.r63p5.mongodb.net/?retryWrites=true&w=majority&appName=MONGO-EH-LIXOO"),
+    getenv('MONGO_URL', ""),
     serverSelectionTimeoutMS=5000    
 )
